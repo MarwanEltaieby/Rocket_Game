11d7c99927380d176ca058c1cedd12929e871d94
Marwan Eltaieby <Marwaneltieby@gmail.com>
2024-03-07 23:50:13 +0200

    Uploading project

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] float delayTime = 1f;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip crash;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem thrustersParticles;
+    [SerializeField] ParticleSystem rightThrustersParticles;
+    [SerializeField] ParticleSystem leftThrustersParticles;
+    private AudioSource audioSource;
+    private bool isTransitioning = false;
+
+    private void Start() {
+        
+    }
+    private void OnCollisionEnter(Collision other) {
+        switch (other.gameObject.tag) {
+            case "Friendly":
+                break;
+
+            case "Finish":
+                SuccessSequence();
+                break;
+
+            default:
+                CrashSequence();
+                break;    
+        }
+    }
+
+    private void SuccessSequence() {
+        if (!isTransitioning) {
+            GetComponent<Movement>().enabled = false;
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Stop();
+            audioSource.PlayOneShot(success);
+            thrustersParticles.Stop();
+            rightThrustersParticles.Stop();
+            leftThrustersParticles.Stop();
+            successParticles.Play();
+            isTransitioning = true;
+            Invoke("LoadNextLevel", delayTime);
+        }
+    }
+
+    private void CrashSequence() {
+        if (!isTransitioning) {
+            GetComponent<Movement>().enabled = false;
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Stop();
+            thrustersParticles.Stop();
+            rightThrustersParticles.Stop();
+            leftThrustersParticles.Stop();
+            crashParticles.Play();
+            audioSource.PlayOneShot(crash);
+            isTransitioning = true;
+            Invoke("ReloadLevel", delayTime);
+        }
+    }
+
+    private void ReloadLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void LoadNextLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody myBody;
+    [SerializeField] float thrustingSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] AudioClip thrusters;
+    [SerializeField] ParticleSystem thrustersParticles;
+    [SerializeField] ParticleSystem rightThrustersParticles;
+    [SerializeField] ParticleSystem leftThrustersParticles;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartThrusting();
+        }
+        else
+        {
+            StopThrusting();
+        }
+    }
+
+    void ProcessRotation() {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            StartRightRotation();
+        }
+        else
+        {
+            StopRightRotation();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            StartLeftRotation();
+        }
+        else
+        {
+            StopLeftRotation();
+        }
+    }
+
+    private void StartThrusting()
+    {
+        myBody.AddRelativeForce(Vector3.up * Time.deltaTime * thrustingSpeed);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(thrusters);
+        }
+        if (!thrustersParticles.isPlaying)
+        {
+            thrustersParticles.Play();
+        }
+    }
+
+    private void StopThrusting()
+    {
+        audioSource.Stop();
+        thrustersParticles.Stop();
+    }
+
+    private void StartLeftRotation()
+    {
+        ApplyRotation(rotationSpeed);
+        if (!rightThrustersParticles.isPlaying)
+        {
+            rightThrustersParticles.Play();
+        }
+    }
+    private void StopLeftRotation()
+    {
+        rightThrustersParticles.Stop();
+    }
+
+    private void StartRightRotation()
+    {
+        ApplyRotation(-rotationSpeed);
+        if (!leftThrustersParticles.isPlaying)
+        {
+            leftThrustersParticles.Play();
+        }
+    }
+    
+    private void StopRightRotation()
+    {
+        leftThrustersParticles.Stop();
+    }
+
+    private void ApplyRotation(float rotationSpeed)
+    {
+        myBody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
+        myBody.freezeRotation = false;
+    }
+}
